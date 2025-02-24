dde3a1f3de35b89751edcfe384970f8a3efaedd7
Yeisson Villamil <yeisson.villamil@globalmvm.com>
2025-02-24 12:34:47 -0500

    Manejo de sql

--- clientes.sql
+++ clientes.sql
@@ -1,4 +1,14 @@
-use ApiRestFul;
+--verificar duplicador
+--select SalesOrderNumber, count(*) as Repetidos
+--from SalesOrder
+--group by SalesOrderNumber
+--having count(*) > 1
+--order by 2 desc
 
-select *
-from SalesOrder
+--Eliminar duplicados
+--with CTE as (
+  --  select *,
+   -- row_number() over (partition by SalesOrderNumber order by SalesOrderLineNumber) as rn
+    --from SalesOrder
+--)
+--delete from CTE WHERE rn > 1
